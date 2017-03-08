3077f8c041d4d8c3bf750850020500f15f49a641
Archit Date <architdate@gmail.com>
2017-03-08 11:36:25 +0800

    Testing Item pickup

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public GameObject particle;
+
+    [SerializeField]
+    private float speed;
+    bool started;
+    bool gameOver;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        //rb.velocity = new Vector3(speed, 0, 0);
+        started = false;
+        gameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!started)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb.velocity = new Vector3(speed, 0, 0);
+                started = true;
+
+                GameManager.instance.StartGame();
+            }
+        }
+
+        //Debug.DrawRay(transform.position, Vector3.down, Color.red);
+
+        if (!Physics.Raycast(transform.position, Vector3.down, 1f))
+        {
+            gameOver = true;
+            rb.velocity = new Vector3(0, -25f, 0);
+
+            Camera.main.GetComponent<CameraFollow>().gameOver = true;
+
+            GameManager.instance.GameOver();
+        }
+
+        if (Input.GetMouseButtonDown(0) && !gameOver)
+        {
+            SwitchDirection();
+        }
+	}
+
+    void SwitchDirection()
+    {
+        if (rb.velocity.z > 0)
+        {
+            rb.velocity = new Vector3(speed, 0, 0);
+        }else if(rb.velocity.x > 0)
+        {
+            rb.velocity = new Vector3(0, 0, speed);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Diamond")
+        {
+            GameObject part = Instantiate(particle, other.gameObject.transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(part, 1f);
+            ScoreManager.instance.startScore();
+            
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+    public bool gameOver;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        gameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        UiManager.instance.GameStart();
+        //ScoreManager.instance.startScore();
+        GameObject.Find("PlatformSpawner").GetComponent<PlatformSpawner>().startSpawiningPlatforms();
+    }
+
+    public void GameOver()
+    {
+        UiManager.instance.GameOver();
+        ScoreManager.instance.StopScore();
+        gameOver = true;
+    }
+}
